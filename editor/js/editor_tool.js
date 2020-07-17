a805262d98ec70da73ad6028180bc17bd50250d1
Calculus <tahar.ayoob@gmail.com>
2020-07-16 18:02:05 -0700

    Update

---
+++ editor/js/editor_tool.js
@@ -0,0 +1,170 @@
+var _0x3a42 = ["\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72", "\x62\x6C\x61\x63\x6B", "\x67\x72\x61\x79", "\x70\x6F\x77\x65\x72\x75\x70\x73", "\x6D\x6F\x64\x65", "\x63\x68\x61\x6E\x67\x65", "\x67\x65\x74\x54\x79\x70\x65", "\x65\x6E\x63\x6F\x64\x65", "\x66\x6C\x6F\x6F\x72", "\x64\x65\x63\x6F\x64\x65", "\x65\x78\x70\x6F\x72\x74", "\x23", "\x6C\x69\x6E\x65", "\x69\x73\x41\x72\x72\x61\x79", "\x6C\x65\x6E\x67\x74\x68", "\x70\x75\x73\x68", "\x20", "\x6A\x6F\x69\x6E", "\x70\x6F\x77\x65\x72\x75\x70", "\x69\x6D\x70\x6F\x72\x74", "\x6D\x61\x74\x63\x68", "\x49\x6E\x76\x61\x6C\x69\x64\x20\x74\x72\x61\x63\x6B\x20\x64\x65\x74\x65\x63\x74\x65\x64\x2E", "\x73\x70\x6C\x69\x74", "", "\x2C", "\x74\x72\x61\x6E\x73\x66\x6F\x72\x6D", "\x72\x6F\x74\x61\x74\x65", "\x50\x49", "\x74\x72\x61\x6E\x73\x6C\x61\x74\x65", "\x73\x63\x61\x6C\x65", "\x63\x6F\x73", "\x73\x69\x6E", "\x66\x6F\x63\x75\x73", "\x23\x69\x6E\x70\x75\x74", "\x63\x6C\x69\x63\x6B", "\x76\x61\x6C\x75\x65", "\x54\x72\x61\x63\x6B\x20\x6E\x6F\x74\x20\x66\x6F\x75\x6E\x64\x2E", "\x23\x73\x63\x61\x6C\x65\x2D\x78", "\x23\x73\x63\x61\x6C\x65\x2D\x79", "\x23\x72\x6F\x74\x61\x74\x65", "\x23\x6D\x6F\x76\x65\x2D\x78", "\x23\x6D\x6F\x76\x65\x2D\x79", "\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C", "\x23\x63\x68\x61\x72\x73", "\x72\x6F\x75\x6E\x64", "\x23\x6F\x75\x74\x70\x75\x74", "\x73\x65\x6C\x65\x63\x74", "\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72", "\x23\x74\x72\x61\x6E\x73\x66\x6F\x72\x6D"];
+(function() {
+    var _0xc964x1 = function(_0xc964x2) {
+        return document[_0x3a42[0]](_0xc964x2)
+    };
+    var _0xc964x3 = function() {
+        this[_0x3a42[1]] = [];
+        this[_0x3a42[2]] = [];
+        this[_0x3a42[3]] = [];
+        this[_0x3a42[4]] = 0;
+        this[_0x3a42[5]] = function(_0xc964x4) {
+            this[_0x3a42[4]] = this[_0x3a42[4]] === 0 ? 1 : 0
+        }
+        ;
+        this[_0x3a42[6]] = function() {
+            return this[_0x3a42[4]] === 0 ? this[_0x3a42[1]] : this[_0x3a42[2]]
+        }
+        ;
+        this[_0x3a42[7]] = function(_0xc964x4) {
+            return parseInt(Math[_0x3a42[8]](_0xc964x4)).toString(32)
+        }
+        ;
+        this[_0x3a42[9]] = function(_0xc964x2) {
+            return parseInt(parseInt(_0xc964x2, 32).toString())
+        }
+        ;
+        this[_0x3a42[10]] = function() {
+            return this[_0x3a42[1]] + _0x3a42[11] + this[_0x3a42[2]] + _0x3a42[11] + this[_0x3a42[3]]
+        }
+        ;
+        this[_0x3a42[12]] = function() {
+            var _0xc964x5 = Array[_0x3a42[13]](arguments[0]) ? arguments[0] : arguments;
+            var _0xc964x6 = [];
+            for (let _0xc964x7 = 0; _0xc964x7 < _0xc964x5[_0x3a42[14]]; _0xc964x7++) {
+                _0xc964x6[_0x3a42[15]](this[_0x3a42[7]](_0xc964x5[_0xc964x7]))
+            }
+            ;this[_0x3a42[6]]()[_0x3a42[15]](_0xc964x6[_0x3a42[17]](_0x3a42[16]));
+            return this
+        }
+        ;
+        this[_0x3a42[18]] = function() {
+            var _0xc964x5 = Array[_0x3a42[13]](arguments[0]) ? arguments[0] : arguments;
+            var _0xc964x6 = [_0xc964x5[0]];
+            for (let _0xc964x7 = 1; _0xc964x7 < _0xc964x5[_0x3a42[14]]; _0xc964x7++) {
+                _0xc964x6[_0x3a42[15]](this[_0x3a42[7]](_0xc964x5[_0xc964x7]))
+            }
+            ;this[_0x3a42[3]][_0x3a42[15]](_0xc964x6[_0x3a42[17]](_0x3a42[16]));
+            return this
+        }
+        ;
+        this[_0x3a42[19]] = function(_0xc964x2) {
+            if (!_0xc964x2[_0x3a42[20]](/\#/g) || _0xc964x2[_0x3a42[20]](/\#/g)[_0x3a42[14]] != 2) {
+                throw new Error(_0x3a42[21])
+            }
+            ;if (_0xc964x2[_0x3a42[10]]) {
+                _0xc964x2 = _0xc964x2[_0x3a42[10]]()
+            }
+            ;_0xc964x2 = _0xc964x2[_0x3a42[22]](_0x3a42[11]);
+            var _0xc964x8 = this[_0x3a42[4]];
+            this[_0x3a42[4]] = 0;
+            for (let _0xc964x7 = 0; _0xc964x7 < _0xc964x2[_0x3a42[14]]; _0xc964x7++) {
+                if (_0xc964x2[_0xc964x7] !== _0x3a42[23]) {
+                    _0xc964x2[_0xc964x7] = _0xc964x2[_0xc964x7][_0x3a42[22]](_0x3a42[24])
+                }
+            }
+            ;for (let _0xc964x4 = 0; _0xc964x4 < 2; _0xc964x4++) {
+                for (let _0xc964x7 = 0; _0xc964x7 < _0xc964x2[_0xc964x4][_0x3a42[14]]; _0xc964x7++) {
+                    let _0xc964x6 = [];
+                    let _0xc964x5 = _0xc964x2[_0xc964x4][_0xc964x7][_0x3a42[22]](_0x3a42[16]);
+                    for (let _0xc964x9 = 0; _0xc964x9 < _0xc964x5[_0x3a42[14]]; _0xc964x9++) {
+                        _0xc964x6[_0x3a42[15]](this[_0x3a42[9]](_0xc964x5[_0xc964x9]))
+                    }
+                    ;this[_0x3a42[12]](_0xc964x6)
+                }
+                ;this[_0x3a42[5]]()
+            }
+            ;for (let _0xc964x7 = 0; _0xc964x7 < _0xc964x2[2][_0x3a42[14]]; _0xc964x7++) {
+                let _0xc964x5 = _0xc964x2[2][_0xc964x7][_0x3a42[22]](_0x3a42[16]);
+                let _0xc964x6 = [_0xc964x5[0]];
+                for (let _0xc964x9 = 1; _0xc964x9 < _0xc964x5[_0x3a42[14]]; _0xc964x9++) {
+                    _0xc964x6[_0x3a42[15]](this[_0x3a42[9]](_0xc964x5[_0xc964x9]))
+                }
+                ;this[_0x3a42[18]](_0xc964x6)
+            }
+            ;this[_0x3a42[4]] = _0xc964x8;
+            return this
+        }
+        ;
+        this[_0x3a42[25]] = function(_0xc964xa, _0xc964x9, _0xc964xb) {
+            if (_0xc964xa == _0x3a42[26]) {
+                _0xc964x9 *= Math[_0x3a42[27]] / 180
+            }
+            ;var _0xc964x2 = this[_0x3a42[10]]()[_0x3a42[22]](_0x3a42[11]);
+            for (let _0xc964x7 = 0; _0xc964x7 < _0xc964x2[_0x3a42[14]]; _0xc964x7++) {
+                if (_0xc964x2[_0xc964x7] !== _0x3a42[23]) {
+                    _0xc964x2[_0xc964x7] = _0xc964x2[_0xc964x7][_0x3a42[22]](_0x3a42[24])
+                }
+            }
+            ;for (let _0xc964x4 = 0; _0xc964x4 < 2; _0xc964x4++) {
+                for (let _0xc964x7 = 0; _0xc964x7 < _0xc964x2[_0xc964x4][_0x3a42[14]]; _0xc964x7++) {
+                    _0xc964x2[_0xc964x4][_0xc964x7] = _0xc964x2[_0xc964x4][_0xc964x7][_0x3a42[22]](_0x3a42[16]);
+                    for (let _0xc964x5 = 0; _0xc964x5 < _0xc964x2[_0xc964x4][_0xc964x7][_0x3a42[14]] - 1; _0xc964x5 += 2) {
+                        switch (_0xc964xa) {
+                        case _0x3a42[28]:
+                            _0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5] = this[_0x3a42[7]](this[_0x3a42[9]](_0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5]) + _0xc964x9);
+                            _0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5 + 1] = this[_0x3a42[7]](this[_0x3a42[9]](_0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5 + 1]) + _0xc964xb);
+                            break;
+                        case _0x3a42[29]:
+                            _0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5] = this[_0x3a42[7]](this[_0x3a42[9]](_0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5]) * _0xc964x9);
+                            _0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5 + 1] = this[_0x3a42[7]](this[_0x3a42[9]](_0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5 + 1]) * _0xc964xb);
+                            break;
+                        case _0x3a42[26]:
+                            let _0xc964xc = this[_0x3a42[9]](_0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5]);
+                            let _0xc964xd = this[_0x3a42[9]](_0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5 + 1]);
+                            _0xc964xc = _0xc964xc * Math[_0x3a42[30]](_0xc964x9) + _0xc964xd * Math[_0x3a42[31]](_0xc964x9);
+                            _0xc964xd = _0xc964xd * Math[_0x3a42[30]](_0xc964x9) - _0xc964xc * Math[_0x3a42[31]](_0xc964x9);
+                            _0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5] = this[_0x3a42[7]](_0xc964xc);
+                            _0xc964x2[_0xc964x4][_0xc964x7][_0xc964x5 + 1] = this[_0x3a42[7]](_0xc964xd)
+                        }
+                    }
+                    ;_0xc964x2[_0xc964x4][_0xc964x7] = _0xc964x2[_0xc964x4][_0xc964x7][_0x3a42[17]](_0x3a42[16])
+                }
+            }
+            ;for (let _0xc964x7 = 0; _0xc964x7 < _0xc964x2[2][_0x3a42[14]]; _0xc964x7++) {
+                _0xc964x2[2][_0xc964x7] = _0xc964x2[2][_0xc964x7][_0x3a42[22]](_0x3a42[16]);
+                switch (_0xc964xa) {
+                case _0x3a42[28]:
+                    _0xc964x2[2][_0xc964x7][1] = this[_0x3a42[7]](this[_0x3a42[9]](_0xc964x2[2][_0xc964x7][1]) + _0xc964x9);
+                    _0xc964x2[2][_0xc964x7][2] = this[_0x3a42[7]](this[_0x3a42[9]](_0xc964x2[2][_0xc964x7][2]) + _0xc964xb);
+                    break;
+                case _0x3a42[29]:
+                    _0xc964x2[2][_0xc964x7][1] = this[_0x3a42[7]](this[_0x3a42[9]](_0xc964x2[2][_0xc964x7][1]) * _0xc964x9);
+                    _0xc964x2[2][_0xc964x7][2] = this[_0x3a42[7]](this[_0x3a42[9]](_0xc964x2[2][_0xc964x7][2]) * _0xc964xb);
+                    break;
+                case _0x3a42[26]:
+                    let _0xc964xc = this[_0x3a42[9]](_0xc964x2[2][_0xc964x7][1]);
+                    let _0xc964xd = this[_0x3a42[9]](_0xc964x2[2][_0xc964x7][2]);
+                    _0xc964xc = _0xc964xc * Math[_0x3a42[30]](_0xc964x9) + _0xc964xd * Math[_0x3a42[31]](_0xc964x9);
+                    _0xc964xd = _0xc964xd * Math[_0x3a42[30]](_0xc964x9) - _0xc964xc * Math[_0x3a42[31]](_0xc964x9);
+                    _0xc964x2[2][_0xc964x7][1] = this[_0x3a42[7]](_0xc964xc);
+                    _0xc964x2[2][_0xc964x7][2] = this[_0x3a42[7]](_0xc964xd)
+                }
+                ;_0xc964x2[2][_0xc964x7] = _0xc964x2[2][_0xc964x7][_0x3a42[17]](_0x3a42[16])
+            }
+            ;for (let _0xc964x7 = 0; _0xc964x7 < _0xc964x2[_0x3a42[14]]; _0xc964x7++) {
+                if (Array[_0x3a42[13]](_0xc964x2[_0xc964x7])) {
+                    _0xc964x2[_0xc964x7] = _0xc964x2[_0xc964x7][_0x3a42[17]](_0x3a42[24])
+                }
+            }
+            ;this[_0x3a42[1]] = _0xc964x2[0];
+            this[_0x3a42[2]] = _0xc964x2[1];
+            this[_0x3a42[3]] = _0xc964x2[2];
+            return this
+        }
+    };
+    _0xc964x1(_0x3a42[33])[_0x3a42[32]]();
+    _0xc964x1(_0x3a42[48])[_0x3a42[47]](_0x3a42[34], function() {
+        var _0xc964xe = new _0xc964x3();
+        if (_0xc964x1(_0x3a42[33])[_0x3a42[35]] == _0x3a42[23]) {
+            throw new Error(_0x3a42[36])
+        }
+        ;_0xc964xe[_0x3a42[19]](_0xc964x1(_0x3a42[33])[_0x3a42[35]]);
+        _0xc964xe[_0x3a42[25]](_0x3a42[29], parseFloat(_0xc964x1(_0x3a42[37])[_0x3a42[35]]), parseFloat(_0xc964x1(_0x3a42[38])[_0x3a42[35]]));
+        _0xc964xe[_0x3a42[25]](_0x3a42[26], parseFloat(_0xc964x1(_0x3a42[39])[_0x3a42[35]]));
+        _0xc964xe[_0x3a42[25]](_0x3a42[28], parseFloat(_0xc964x1(_0x3a42[40])[_0x3a42[35]]), parseFloat(_0xc964x1(_0x3a42[41])[_0x3a42[35]]));
+        _0xc964x1(_0x3a42[43])[_0x3a42[42]] = Math[_0x3a42[44]](_0xc964xe[_0x3a42[10]]()[_0x3a42[14]] / 10) / 100;
+        _0xc964x1(_0x3a42[45])[_0x3a42[35]] = _0xc964xe[_0x3a42[10]]();
+        _0xc964x1(_0x3a42[45])[_0x3a42[46]]()
+    }, false)
+}())
