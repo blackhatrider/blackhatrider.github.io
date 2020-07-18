e36f0988d1b68e921659fc81b8ef5a8059d90c29
Calculus <tahar.ayoob@gmail.com>
2020-07-18 12:43:14 -0700

    Update 2.6
    Minor Changes

--- tracks/js/test.js
+++ tracks/js/test.js
@@ -1,8 +1,18 @@
-function ajax(url, callback) {
+function ajaxCode(url, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             callback(JSON.parse(xmlhttp.responseText.slice(2,-2)).code);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+function ajaxTitle(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            callback(JSON.parse(xmlhttp.responseText.slice(2,-2)).title);
         }
     };
     xmlhttp.open("GET", url, true);
@@ -15,13 +25,16 @@ function ParseURLParameter(p){
         return c[1]	
     }
 }
-async function test(){
+async function Inject(){
     var p = ParseURLParameter('id');
     if(typeof p != 'undefined'){
-        const t = await new Promise(x => ajax(`https://cors-anywhere.herokuapp.com/https://cdn.freeriderhd.com/free_rider_hd/tracks/prd/${p}/track-data-v1.js`, x))
+        const code = await new Promise(x => ajaxCode(`https://cors-anywhere.herokuapp.com/https://cdn.freeriderhd.com/free_rider_hd/tracks/prd/${p}/track-data-v1.js`, x))
         let inject = document.createElement('script');
-        inject.innerHTML = `(BH.game || cr).ride('${t}')`
+        inject.innerHTML = `(BH.game || cr).ride('${code}')`
         document.head.appendChild(inject);
+        const title = await new Promise(x => ajaxTitle(`https://cors-anywhere.herokuapp.com/https://cdn.freeriderhd.com/free_rider_hd/tracks/prd/${p}/track-data-v1.js`, x))
+        document.getElementsByTagName("title")[0].innerHTML = `${title} | Black Hat Rider 2`
+        console.log(title)
     }
 };
-test()
+Inject()
