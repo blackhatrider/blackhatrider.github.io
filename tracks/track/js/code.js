26d9790a14e60dad365ed95d17ac64ecc516514d
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-06-01 07:53:40 -0700

    Update code.js

--- tracks/track/js/code.js
+++ tracks/track/js/code.js
@@ -1,5 +1,5 @@
 function ajax(url, callback) {
-    var xmlhttp = new XMLHttpRequest();
+    let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             callback(JSON.parse(xmlhttp.responseText.slice(2,-2)));
@@ -9,7 +9,7 @@ function ajax(url, callback) {
     xmlhttp.send();
 }
 function ajaxId(url, callback) {
-    var xmlhttp = new XMLHttpRequest();
+    let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             callback(JSON.parse(xmlhttp.responseText));
@@ -18,15 +18,13 @@ function ajaxId(url, callback) {
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
-function ParseURLParameter(p){
-    var m = window.location.search.substring(1);
-    var c = m.split('=');
-    if(c[0] == p){
-        return c[1]	
-    }
+function parseURLParameter(t){
+    const e = window.location.search.substring(1).split(/\u0026/g).map(t => t.split(/\u003D/g));
+    const i = e.find(e => e[0] == t);
+    return i?.[1];
 }
 async function Inject(){
-    var p = ParseURLParameter('id');
+    let p = parseURLParameter('id');
     if(typeof p != 'undefined'){
         if(p != 'random'){
             const data = await new Promise(x => ajax(`https://cors-anywhere.herokuapp.com/https://cdn.freeriderhd.com/free_rider_hd/tracks/prd/${p}/track-data-v1.js`, x))
