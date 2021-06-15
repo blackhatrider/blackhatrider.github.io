6c9c54048d74ec73d3c82f453cf2023db57f49dc
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-06-15 13:30:07 -0700

    Update init.js

--- init.js
+++ init.js
@@ -4,14 +4,14 @@ function parseURLParameter(t){
     return i?.[1];
 }
 
-fetch("/header.html").then(async response => (document.querySelector("header").innerHTML = await response.text())).then(t => {
+fetch("/header.html").then(async response => (document.querySelector("header").innerHTML = await response.text()));
+
+fetch("/footer.html").then(async response => (document.querySelector("footer").innerHTML = await response.text())).then(t => {
     const link = document.createElement("link");
     link.href = JSON.parse(localStorage.dark) ? "/dark.css" : "/light.css";
     link.rel = "stylesheet";
     document.head.appendChild(link);
-})
-
-fetch("/footer.html").then(async response => (document.querySelector("footer").innerHTML = await response.text())).then(t => {
+    
     const dark = document.getElementById("dark");
     if (JSON.parse(localStorage.dark)) {
         dark.checked = true;
