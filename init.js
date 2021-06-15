d4eab0c8b303613c0629b1db774b5ccfb1f71192
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-06-15 14:25:26 -0700

    Update init.js

--- init.js
+++ init.js
@@ -1,11 +1,6 @@
 import users from "../users.js";
 
-for (const t of users) {
-    if (localStorage.getItem("username") && localStorage.getItem("password") && t.user != localStorage.getItem("username") && t.key == localStorage.getItem("password")) continue;
-    if (t.user == localStorage.getItem("username") && t.key == localStorage.getItem("password")) break;
-    if (location.pathname != "/default.html") location.href = "/default.html";
-}
-//if (!localStorage.getItem("username") || !localStorage.getItem("password")) return location.href = "/default.html";
+if (!localStorage.getItem("username") || !localStorage.getItem("password") || users.find(t => t.user == localStorage.getItem("username") && t.key == localStorage.getItem("password"))) location.href = "/default.html";
 
 function parseURLParameter(t){
     const e = window.location.search.substring(1).split(/\u0026/g).map(t => t.split(/\u003D/g));
